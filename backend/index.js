2c40217ea053519d45163d184e9d411b14fde857
Stefan Trajkovski <trajkovski.stefan@hotmail.com>
2025-07-01 14:27:41 +0200

    sss

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const PORT = 3001;
+const port = process.env.PORT || 3000;
 
 app.use(cors()); // allow cross-origin requests from frontend
 app.use(express.json()); // parse JSON bodies
